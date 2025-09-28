d0fc7b0f4562dd861804b164a524f77dd41cdbff
Sanya .sharma <sharma.sanya3020@gmail.com>
2025-09-28 19:15:37 +0530

    Add files via upload

--- Java/binarysearch.java
+++ Java/binarysearch.java
@@ -1,19 +1,18 @@
-
-
 public class binarysearch {
     public static void main(String[] args) {
-        int arr[] = { -18, -12, -4, 0, 2, 3, 4, 15, 19, 20, 45, 49, 56, 77, 89 };
-        int target = 19;
-        int ans = binarySearch(arr, target);
+        int[] arr = { -18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89 };
+        int target = 9;
+        int ans = binarysearch(arr, target);
         System.out.println(ans);
     }
 
-    static int binarySearch(int arr[], int target) {
+    static int binarysearch(int[] arr, int target) {
         int start = 0;
-        int end = arr.length - 1;
+        int end = arr.length;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
+
             if (target < arr[mid]) {
                 end = mid - 1;
             } else if (target > arr[mid]) {
@@ -21,7 +20,9 @@ public class binarysearch {
             } else {
                 return mid;
             }
+
         }
-        return -1; // return -1 if target not found
+        return -1;
     }
+
 }
